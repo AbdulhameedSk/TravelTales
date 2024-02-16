6611dfd2d12b476aa326bd44ddecd7ed010754f0
ABDULHAMEED <skshaikabdulhameed786@gmail.com>
2024-02-16 09:39:56 +0530

    Added Header

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -4,35 +4,46 @@ import { Box, AppBar, Toolbar, Button, Typography } from "@mui/material";
 const Header = () => {
   return (
     <>
-      <AppBar position="sticky" style={{ backgroundColor: "#111" }}>
+      <AppBar position="sticky" style={{ backgroundColor: "black" }}>
         <Toolbar>
           <Typography
             variant="h5"
-            style={{ flexGrow: 1, fontWeight: "bold", letterSpacing: "1px" }}
+            style={{ flexGrow: 1, fontWeight: "bold", letterSpacing: "1px", fontSize:"30px" }}
           >
             TravelTales
           </Typography>
           <Box>
             <Button
+              className="header-button"
               variant="contained"
               color="primary"
               style={{ margin: "0 10px" }}
             >
-              Login
+              Home
             </Button>
             <Button
               variant="contained"
+              className="header-button"
               color="primary"
               style={{ margin: "0 10px" }}
             >
-              Register
+              Destinations
             </Button>
             <Button
+             className="header-button"
               variant="contained"
               color="primary"
               style={{ margin: "0 10px" }}
             >
-              Logout
+              My Blogs
+            </Button>
+            <Button
+             className="header-button"
+              variant="contained"
+              color="primary"
+              style={{ margin: "0 10px" }}
+            >
+              Contact
             </Button>
           </Box>
         </Toolbar>
